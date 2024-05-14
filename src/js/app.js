9ae30f2f7724b4f9bb9e96151fc4077653c1d76a
Papa-Pedro-13 <aleksandrostrovsqi@gmail.com>
2024-05-14 10:31:09 +0300

    3 new pages

--- src/js/app.js
+++ src/js/app.js
@@ -62,16 +62,49 @@ var swiper = new Swiper(".prod-slider", {
 new Swiper(".trending__slider", {
 	slidesPerView: "auto",
 	spaceBetween: 16,
-	response: {
+	breakpoints: {
 		1380: {
 			slidesPerView: 3,
 			spaceBetween: 24,
-
 		}
 	},
 	navigation: {
 		nextEl: ".trending-slider-next",
 		prevEl: ".trending-slider-prev",
+	},
+});
+var swiper2 = new Swiper(".mySwiper3", {
+	direction: "vertical",
+	spaceBetween: 16,
+	slidesPerView: 4,
+	freeMode: true,
+	watchSlidesProgress: true,
+});
+new Swiper(".products__list", {
+	navigation: {
+		nextEl: ".products__next",
+		prevEl: ".products__prev",
+	},
+	slidesPerView: 2,
+	spaceBetween: 8,
+	grid: {
+		rows: 3,
+	},
+	breakpoints: {
+		768: {
+			spaceBetween: 16,
+		},
+		1380: {
+			slidesPerView: 3,
+			spaceBetween: 24,
+		}
+	},
+	pagination: {
+		el: ".products__pagination",
+		clickable: true,
+		renderBullet: function (index, className) {
+			return '<span class="' + className + '">' + (index + 1) + "</span>";
+		},
 	},
 });
 
@@ -93,8 +126,37 @@ function toggleBodyLock2(isLock) {
 	html.classList.toggle('lock', isLock);
 }
 
-
 window.onload = function () {
+	document.querySelector(".products__filter-btn").addEventListener('click', () => {
+		document.querySelector(".products__filters").classList.add('active')
+		toggleBodyLock2(true);
+	})
+	document.querySelector(".products-filters__close").addEventListener('click', () => {
+		document.querySelector(".products__filters").classList.remove('active')
+		if (!html.classList.contains('menu-open')) {
+			toggleBodyLock2(false)
+		}
+	})
+	document.querySelector(".products__result").addEventListener('click', () => {
+		document.querySelector(".products__filters").classList.remove('active')
+		if (!html.classList.contains('menu-open')) {
+			toggleBodyLock2(false)
+		}
+	})
+	document.querySelector(".products__filters").addEventListener('click', (e) => {
+		if (e.target === document.querySelector(".products__filters")) {
+			document.querySelector(".products__filters").classList.remove('active')
+			if (!html.classList.contains('menu-open')) {
+				toggleBodyLock2(false)
+			}
+		}
+	})
+	document.querySelector(".products__clear").addEventListener('click', () => {
+		document.querySelector(".products__filters").classList.remove('active')
+		if (!html.classList.contains('menu-open')) {
+			toggleBodyLock2(false)
+		}
+	})
 	document.querySelectorAll(".dropdown__btn").forEach((item) => {
 		item.addEventListener('click', (e) => {
 			if (item.parentElement.classList.contains('active')) {
@@ -148,6 +210,93 @@ window.onload = function () {
 			})
 		})
 	})
+	try {
+		customSelect();
+	}
+	catch (err) { console.log(err) }
+}
+
+function customSelect() {
+	var x, i, j, l, ll, selElmnt, a, b, c;
+	/* Look for any elements with the class "products__select": */
+	x = document.getElementsByClassName("products__select");
+	l = x.length;
+	for (i = 0; i < l; i++) {
+		selElmnt = x[i].getElementsByTagName("select")[0];
+		ll = selElmnt.length;
+		/* For each element, create a new DIV that will act as the selected item: */
+		a = document.createElement("DIV");
+		a.setAttribute("class", "select-selected");
+		a.innerHTML = selElmnt.options[selElmnt.selectedIndex].innerHTML;
+		x[i].appendChild(a);
+		/* For each element, create a new DIV that will contain the option list: */
+		b = document.createElement("DIV");
+		b.setAttribute("class", "select-items select-hide");
+		for (j = 1; j < ll; j++) {
+			/* For each option in the original select element,
+			create a new DIV that will act as an option item: */
+			c = document.createElement("DIV");
+			c.innerHTML = selElmnt.options[j].innerHTML;
+			c.addEventListener("click", function (e) {
+				/* When an item is clicked, update the original select box,
+				and the selected item: */
+				var y, i, k, s, h, sl, yl;
+				s = this.parentNode.parentNode.getElementsByTagName("select")[0];
+				sl = s.length;
+				h = this.parentNode.previousSibling;
+				for (i = 0; i < sl; i++) {
+					if (s.options[i].innerHTML == this.innerHTML) {
+						s.selectedIndex = i;
+						h.innerHTML = this.innerHTML;
+						y = this.parentNode.getElementsByClassName("same-as-selected");
+						yl = y.length;
+						for (k = 0; k < yl; k++) {
+							y[k].removeAttribute("class");
+						}
+						this.setAttribute("class", "same-as-selected");
+						break;
+					}
+				}
+				h.click();
+			});
+			b.appendChild(c);
+		}
+		x[i].appendChild(b);
+		a.addEventListener("click", function (e) {
+			/* When the select box is clicked, close any other select boxes,
+			and open/close the current select box: */
+			e.stopPropagation();
+			closeAllSelect(this);
+			this.nextSibling.classList.toggle("select-hide");
+			this.classList.toggle("select-arrow-active");
+		});
+	}
+
+	function closeAllSelect(elmnt) {
+		/* A function that will close all select boxes in the document,
+		except the current select box: */
+		var x, y, i, xl, yl, arrNo = [];
+		x = document.getElementsByClassName("select-items");
+		y = document.getElementsByClassName("select-selected");
+		xl = x.length;
+		yl = y.length;
+		for (i = 0; i < yl; i++) {
+			if (elmnt == y[i]) {
+				arrNo.push(i)
+			} else {
+				y[i].classList.remove("select-arrow-active");
+			}
+		}
+		for (i = 0; i < xl; i++) {
+			if (arrNo.indexOf(i)) {
+				x[i].classList.add("select-hide");
+			}
+		}
+	}
+
+	/* If the user clicks anywhere outside the select box,
+	then close all select boxes: */
+	document.addEventListener("click", closeAllSelect);
 }
 
 // new Tabs('tabs-example', {
@@ -156,6 +305,16 @@ window.onload = function () {
 // 	},
 // });
 
+new Accordion('.products__accordion', {
+	shouldOpenAll: false, // true
+	defaultOpen: [0, 1], // [0,1]
+	collapsedClass: 'open',
+});
+new Accordion('.product__accordion', {
+	shouldOpenAll: true, // true
+	defaultOpen: [0, 1], // [0,1]
+	collapsedClass: 'open',
+});
 new Accordion('.accordion', {
 	shouldOpenAll: false, // true
 	defaultOpen: [], // [0,1]
